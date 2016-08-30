ded250abbe6f8252a54419b62cb9299c4134cfc9
Brad G <Brad G>
2016-08-30 12:18:38 -0500

    work on part 1

--- fixed.c
+++ fixed.c
@@ -1,20 +1,40 @@
+//Joe Retter, 
+//Brad Gray, bg22946
+//EE 445L MW 5-630PM
+//Lab 1
+
 #include <stdint.h>
 #include <stdio.h>
 #include "ST7735.h" //added prototype for fputc on last line of this header
 
+#define MAX_PARAM 9999
+#define MIN_PARAM -9999
+
 void ST7735_sDecOut3(int32_t x) {
-	int sign_character;
-	int first_digit;
-	int second_digit;
-	int third_digit;
-	int fourth_digit;
-	int running_dividend;
+	int ones = 0, tenths = 0, hundredths = 0, thousandths = 0;
+	int temp_val;
+	FILE *fp;
+	char sign = ' ';
+	char decimal = '.';
+	char str[7];
 	
-	if(x < 0) {
-		
-		running_dividend = x / 10;
-		fourth_digit = x % 10; 
-		fputc(fourth_digit, ); //not sure about the file pointer... we don't need it
+	if(x < 0){
+		sign = '-';
+		x = - x;
+	}
+	
+	if(x > MIN_PARAM && x < MAX_PARAM) {
+		temp_val = x / 10;
+		thousandths = x % 10;
+		hundredths = temp_val % 10;
+		temp_val = temp_val / 10;
+		tenths = temp_val % 10;
+		temp_val = temp_val / 10;
+		ones = temp_val / 10;
+		printf("%c%d%c%d%d%d\n", sign, ones, decimal, tenths, hundredths, thousandths);
+		//fputc(str, fp); // using dummy file pointer
+	}else {
+		printf(" *.***\n");
 	}
 	
 }
--- fixed.h
+++ fixed.h
@@ -1,3 +1,8 @@
+//Joe Retter, 
+//Brad Gray, bg22946
+//EE 445L MW 5-630PM
+//Lab 1
+
 void ST7735_sDecOut3(int32_t x); //signed 32-bit decimal fixed-point delta=0.001
 void ST7735_uBinOut8(uint32_t y); //unsigned 32-bit binary fixed-point delta=1/256
 void ST7735_XYplotInit(); //specify the X and Y axes for an x-y scatter plot
