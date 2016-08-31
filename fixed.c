0d01e3ab9afec7256718b712716af8547d3631ae
Brad G <Brad G>
2016-08-30 20:00:05 -0500

    part 2 working

--- fixed.c
+++ fixed.c
@@ -7,26 +7,28 @@
 #include <stdio.h>
 #include "ST7735.h" //added prototype for fputc on last line of this header
 
-#define MAX_PARAM 9999
-#define MIN_PARAM -9999
+#define MAX_PARAM_PART1 9999
+#define MIN_PARAM_PART1 -9999
+#define MAX_PARAM_PART2 256000
 #define SPACE 0x20
 #define NEWLINE 0x0A
+#define DECIMAL 0x2E
+#define STAR 0x2A
+#define ASCII_CONV 0x30;
 
 void ST7735_sDecOut3(int32_t x) {
 	int32_t ones = 0, tenths = 0, hundredths = 0, thousandths = 0;
 	int32_t ones_ascii = 0, tenths_ascii = 0, hundredths_ascii = 0, thousandths_ascii = 0;
 	int32_t temp_val;
 	FILE *fp;
-	int32_t sign = 0x20; //ascii for space
-	int32_t decimal = 0x2E; //ascii for decimal
-	int32_t star = 0x2A; //ascii for *
-
+	int32_t sign = SPACE; //ascii for space
+	
 	if(x < 0){
-		sign = 0x2D;
+		sign = 0x2D; //negative sign
 		x = - x;
 	}
 	
-	if(x > MIN_PARAM && x < MAX_PARAM) {
+	if(x > MIN_PARAM_PART1 && x < MAX_PARAM_PART1) {
 		temp_val = x / 10;
 		thousandths = x % 10;
 		hundredths = temp_val % 10;
@@ -35,26 +37,79 @@ void ST7735_sDecOut3(int32_t x) {
 		temp_val = temp_val / 10;
 		ones = temp_val % 10;
 		
-		ones_ascii = ones + 0x30;
-		tenths_ascii = tenths + 0x30;
-		hundredths_ascii = hundredths + 0x30;
-		thousandths_ascii = thousandths + 0x30;
+		ones_ascii = ones + ASCII_CONV;
+		tenths_ascii = tenths + ASCII_CONV;
+		hundredths_ascii = hundredths + ASCII_CONV;
+		thousandths_ascii = thousandths + ASCII_CONV;
 
 		fputc(sign, fp);
 		fputc(ones_ascii, fp);
-		fputc(decimal, fp);
+		fputc(DECIMAL, fp);
 		fputc(tenths_ascii, fp);
 		fputc(hundredths_ascii, fp);
 		fputc(thousandths_ascii, fp);
 		fputc(NEWLINE, fp);
 	}else {
 		fputc(SPACE, fp);
-		fputc(star, fp);
-		fputc(decimal, fp);
-		fputc(star, fp);
-		fputc(star, fp);
-		fputc(star, fp);
+		fputc(STAR, fp);
+		fputc(DECIMAL, fp);
+		fputc(STAR, fp);
+		fputc(STAR, fp);
+		fputc(STAR, fp);
 		fputc(NEWLINE, fp);
 	}
 	
+}
+
+void ST7735_uBinOut8(uint32_t x) {
+	int whole, fraction, round, ones, tens, hundreds;
+	int tenths, tenths_ascii, hundredths, hundredths_ascii;
+	int ones_ascii, tens_ascii, hundreds_ascii;
+	int resolution = 256; //resolution = 1/256 * 100
+	int conversion = 39; // (1/256)  * 100
+	FILE *fp;
+	int32_t sign = SPACE; //ascii for space
+	
+	//getting numbers right of decimal
+	whole = x >> 8;
+	hundreds = whole / 100;
+	whole = whole - (hundreds * 100);
+	tens = whole / 10;
+	whole = whole - (tens * 10);
+	ones = whole;
+	
+	//getting numbers left of decimal
+	fraction = (x % resolution) * conversion;
+	round = fraction % 100;
+	fraction = fraction / 100;
+	if (round > 49) {
+		fraction++;
+	}
+	hundredths = fraction % 10;
+	tenths = fraction / 10;
+		
+	ones_ascii = ones + ASCII_CONV;
+	tens_ascii = tens + ASCII_CONV;
+	hundreds_ascii = hundreds + ASCII_CONV;
+	hundredths_ascii = hundredths + ASCII_CONV;
+	tenths_ascii = tenths + ASCII_CONV;
+	
+if(x > 0 && x < MAX_PARAM_PART2){	
+		fputc(hundreds_ascii, fp);
+		fputc(tens_ascii, fp);
+		fputc(ones_ascii, fp);
+		fputc(DECIMAL, fp);
+		fputc(tenths_ascii, fp);
+		fputc(hundredths_ascii, fp);
+		fputc(NEWLINE, fp);
+	}else {
+		fputc(STAR, fp);
+		fputc(STAR, fp);
+		fputc(STAR, fp);
+		fputc(DECIMAL, fp);
+		fputc(STAR, fp);
+		fputc(STAR, fp);
+		fputc(STAR, fp);
+		fputc(NEWLINE, fp);
+	}
 }
--- main.c
+++ main.c
@@ -15,4 +15,16 @@ int main(void) {
 	ST7735_InitR(INITR_REDTAB);
 	ST7735_sDecOut3(8912);
 	ST7735_sDecOut3(890980);
+	ST7735_sDecOut3(-2);
+	ST7735_uBinOut8(-64);
+	ST7735_uBinOut8(0);
+	ST7735_uBinOut8(2);
+  ST7735_uBinOut8(64);
+	ST7735_uBinOut8(100);
+	ST7735_uBinOut8(500);
+	ST7735_uBinOut8(512);
+	ST7735_uBinOut8(5000);
+	ST7735_uBinOut8(30000);
+	ST7735_uBinOut8(255997);
+	ST7735_uBinOut8(256000);
 }
